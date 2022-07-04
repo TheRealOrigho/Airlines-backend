e6370caf88e1c1b6799f8d3bae3f45377edb9d69
Kevin Origho <orighokevin@gmail.com>
2022-07-04 16:23:53 +0100

    frst commit for airline projects: python

---
+++ flights/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Airport, Flights, Passenger
+
+# Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
+
+admin.site.register(Airport,)
+admin.site.register(Flights, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
